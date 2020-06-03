a83b19a879adaa29d384dfa1f1e8ce3705e1908e
danieljustus32 <daniel.justus32@gmail.com>
2020-06-02 23:34:32 -0500

    update db config

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'protestupdates'),
+        username: env('DATABASE_USERNAME', 'protestupdates'),
+        password: env('DATABASE_PASSWORD', '123'),
       },
       options: {
         ssl: false,
